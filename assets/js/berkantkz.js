7ca07bf37da0ce672b43ecd30571cb5dc395c77b
Berkant Korkmaz <berkantk3@gmail.com>
2017-11-28 23:20:24 +0300

    Updates

--- assets/js/berkantkz.js
+++ assets/js/berkantkz.js
@@ -48,26 +48,16 @@ var english = new Function('tr','en', 'document.body.innerHTML = document.body.i
 if(window.location.href.indexOf("eng") > -1) {
 	document.documentElement.lang = 'en';
 	english('"Ne mutlu Türk\'üm diyene!"','"How happy the one who says I\'m a Turk!"');
-	english('Sosyal Hesap','Social');
-	english('Instagram\'ı çok sık olmasa da kullanıyorum.','I use Instagram even if it\'s not frequently.');
-	english('Profile git','Go to profile');
+	english('Wikipedia\'dan Wiki Zero\'ya andaş olarak yönlendirme görevini yapan Chromium eklentisi.','A Chromium extension which automatically redirects WikiPedia to Wiki Zero');
 	english('Bilgi','Info');
 	english('Yaşam öyküm','My biography');
 	english('99 yılında Kocaeli\'de doğdum, orada büyüdüm. Şuan Kırklareli Üniversitesi\'nde mütercim tercümanlık bölümünde okuyorum. İstanbul\'dan Fenerbahçe Spor Kulübünü destekliyorum.','I was born in the year 99 in Kocaeli, grew up there. Now, I\'m studying translation-interpretationan at Kırklareli University. I support Fenerbahçe Spor Kulübü among İstanbul clubs.');
 	english('Geliştirme','Development');
-	english('TeamWin Kurtarma Modu','TeamWin Recovery Mode');
-	english('LG\'nin K10 modeli için dünyada yapılmış olan ilk geliştirmem/nin XDA konusu.','XDA thread of my first development that has been made for LG K10 ever.');
-	english('Konuyu gör','See the thread');
 	english('Sosyal Hesap','Social');
-	english('Çalışmalarımı (eğer bir gün yaparsam) yayınladığım adres.','Address where I publish my works (if I get done some someday).');
-	english('Profile git','Go to profile');
 	english('Efsaneler gelir ve giderler ancak sonsuza dek unutulmazlar...','Legends come and go but they are never forgotten...');
 	english('Sosyal Hesap','Social');
 	english('Neredeyse asla girmediğim Twitter.','Account which I almost never visit.');
-	english('Profile git','Go to profile');
 	english('Sosyal Hesap','Social');
-	english('Çok sık kullanılmayan bir Facebook hesabı...','An account which is not used much...');
-	english('Profile git','Go to profile');
 	english('Geliştirme','Development');
 	english('Geliştirip Google Play\'de yayınladığım ilk uygulamam.','The first app I developed and published at Google Play Store.');
 	english('Google Play\'den alın','Get it from Google Play');
